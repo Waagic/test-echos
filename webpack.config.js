c1189e0fefc67cbc9ebebab4b56ca70cc2e3b6c0
Lucas <l.marguiron@extralagence.com>
2023-11-20 09:47:37 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -70,4 +70,11 @@ Encore
     //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();
+const fullConfig = Encore.getWebpackConfig();
+fullConfig.devServer = {
+    watchFiles: {
+        paths: ['templates/**/*.html.twig'],
+    },
+};
+module.exports = fullConfig;
+
